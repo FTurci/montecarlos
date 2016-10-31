4e61ada8957d5737bb1b8f35a836a61c3d1f8ef5
Francesco Turci <f.turci@bristol.ac.uk>
2016-10-31 15:47:48 +0000

    eliminated gsl

--- StandardMC/liquid.h
+++ StandardMC/liquid.h
@@ -134,17 +134,17 @@ class liquid {
 };
 
 
-template< typename F >
-  class gsl_function_pp : public gsl_function {
-  public:
-  gsl_function_pp(const F& func) : _func(func) {
-    function = &gsl_function_pp::invoke;
-    params=this;
-  }
-  private:
-  const F& _func;
-  static double invoke(double x, void *params) {
-    return static_cast<gsl_function_pp*>(params)->_func(x);
-  }
-};
+// template< typename F >
+//   class gsl_function_pp : public gsl_function {
+//   public:
+//   gsl_function_pp(const F& func) : _func(func) {
+//     function = &gsl_function_pp::invoke;
+//     params=this;
+//   }
+//   private:
+//   const F& _func;
+//   static double invoke(double x, void *params) {
+//     return static_cast<gsl_function_pp*>(params)->_func(x);
+//   }
+// };
 #endif
